c783518d2afcd99bc0293004493f15a84c6d5287
Mpho <mselemela10@gmail.com>
2025-06-17 20:16:06 +0200

    strings part2

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 #strings
 
-message = "hello world"
+message = "                     Hello, World,!  "
 
-print(message);
+print(message[0]) #print first character in string
+print(len(message)) #print length of string
+print(message.strip()) # remove leading and trailing white space in string
+print(message.lower()) # convert to lower case
+print(message.split(',')) #split string in a list based on the comma
