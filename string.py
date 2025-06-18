0d32f36866614cb5ee283b3e237d5d23eae26267
Mpho <mselemela10@gmail.com>
2025-06-18 12:45:48 +0200

    control statement

---
+++ string.py
@@ -0,0 +1,20 @@
+# string with oparators
+
+str1 = 'hello'
+str2 = 'world'
+
+print(str1 + " " + str2)
+
+print(str1 * 3)
+
+
+
+#control statement
+num = -10
+
+if num > 0:
+    print("this number is positive")
+elif num == 0:
+    print("this number is zero")
+else:
+    print("this number is negative")
