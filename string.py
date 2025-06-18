99002d63b80e39b8a25c3feca3bada569b51efc3
Mpho <mselemela10@gmail.com>
2025-06-18 19:22:48 +0200

    loops

--- string.py
+++ string.py
@@ -18,3 +18,14 @@ elif num == 0:
     print("this number is zero")
 else:
     print("this number is negative")
+    
+    
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    print(num1, "is geater than", num2)
+elif num2 > num1:
+    print(num2, "is greater than", num1)
+else:
+    print("Both numbers are equal")
