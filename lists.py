273a4f6a0230ccf1254c70b43dbedd9304c249ef
Mpho <mselemela10@gmail.com>
2025-06-29 17:12:35 +0200

    lists part 2

--- lists.py
+++ lists.py
@@ -6,5 +6,24 @@ print(fruits[0])
 
 # Replace banana with blueberry
 fruits[1] = "blueberry"
+print(fruits)
 
+# Add to list
+fruits.append("kiwi")
+print(fruits)
+
+# add to list at a postion 1
+fruits.insert(1, "orange")
+print(fruits)
+
+# Remove item from list
+fruits.remove("kiwi")
+print(fruits)
+
+# sorting list in alphabetical order
+fruits.sort()
+print(fruits)
+
+# sorting list in reverse order
+fruits.sort(reverse=True)
 print(fruits)
